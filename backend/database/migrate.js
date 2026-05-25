39056adac70c7a8f3b0c1d08a47335c96ac401e5
Abolfazl Ganjtabesh <brillianttrussure@gmail.com>
2026-05-25 19:34:24 +0330

    feat(database): add migration script for athlete table
    - create migrate.js to automate table creation
    - define athlete schema with fields: id, name, last_name, phone_number, password
    - add unique constraint to phone_number
    - implement default values for created_at and expire_date using SQL intervals

---
+++ backend/database/migrate.js
@@ -0,0 +1,29 @@
+//database/migrate.js
+const pool = require("../config/db");
+
+async function migrate() {
+  try {
+
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS athlete (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  phone_number TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  expire_date TIMESTAMP NOT NULL DEFAULT (NOW() + INTERVAL '30 days')
+);
+
+    `);
+
+    console.log("✅ Athlete table created");
+
+    await pool.end();
+
+  } catch (error) {
+    console.error("Migration failed:", error);
+  }
+}
+
+migrate();
